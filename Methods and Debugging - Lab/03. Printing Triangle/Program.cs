a4093675689a2bbf442856d0f648e991c1bd1518
svetoslavmishev <svetli>
2017-10-12 00:07:21 +0300

    add fifth homework

---
+++ Methods and Debugging - Lab/03. Printing Triangle/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace _03.Printing_Triangle
+{
+    class Program
+    {
+        static void Main()
+        {
+            int n = int.Parse(Console.ReadLine());
+            PrintTrinagle(n);
+        }
+
+        static void PrintLine(int start, int end)
+        {
+            for (int i = start; i <= end; i++)
+            {
+                Console.Write(i + " ");
+            }
+            Console.WriteLine();
+        }
+
+        static void PrintTrinagle(int n)
+        {
+            for (int i = 1; i <= n; i++)
+            {
+                PrintLine(1, i);
+            }
+
+            for (int i = n - 1; i > 0; i--)
+            {
+                PrintLine(1, i);
+            }
+        }
+    }
+}
+
---
+++ Methods and Debugging - Lab/11. Price Change Alert/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+namespace _11.Price_Change_Alert
+{
+    class PriceChangeAlert
+    {
+        static void Main()
+        {
+            int n = int.Parse(Console.ReadLine());
+            double granica = double.Parse(Console.ReadLine());
+            double last = double.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n - 1; i++)
+            {
+                double c = double.Parse(Console.ReadLine());
+                double div = Proc(last, c);
+                bool isSignificantDifference = isDifference(div, granica);
+                string message = Get(c, last, div, isSignificantDifference);
+                Console.WriteLine(message);
+                //last = c;
+            }
+        }
+
+        static string Get(double c, double last, double difference, bool etherTrueOrFalse)
+        {
+            string to = "";
+            if (difference == 0)
+            {
+                to = string.Format("NO CHANGE: {0}", c);
+            }
+            else if (!etherTrueOrFalse)
+            {
+                to = string.Format("MINOR CHANGE: {0} to {1} ({2:F2}%)", last, c, difference);
+            }
+            else if (etherTrueOrFalse && (difference > 0))
+            {
+                to = string.Format("PRICE UP: {0} to {1} ({2:F2}%)", last, c, difference);
+            }
+            else if (etherTrueOrFalse && (difference < 0))
+                to = string.Format($"PRICE DOWN: {0} to {1} ({2*100:F2}%)", last, c, difference);
+            return to;
+        }
+
+        static bool isDifference(double limit, double isDiff)
+        {
+            if (Math.Abs(limit) >= isDiff)
+            {
+                return true;
+            }
+            return false;
+        }
+
+        static double Proc(double l, double c)
+        {
+            double r = (c - l) / l;
+            return r;
+        }
+    }
+}
+
+
+
