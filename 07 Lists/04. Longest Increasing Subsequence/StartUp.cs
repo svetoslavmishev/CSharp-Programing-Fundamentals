93f95d2ae7c48da5bc18d2b0e653b1f1f0dac28e
svetoslavmishev <svetli>
2017-11-11 11:30:57 +0200

    Lists exercises

---
+++ 07 Lists/04. Longest Increasing Subsequence/StartUp.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _04.Longest_Increasing_Subsequence
+{
+    class StartUp
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
---
+++ 07 Lists/06. Sum Reversed Numbers/StartUp.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _06.Sum_Reversed_Numbers
+{
+    class StartUp
+    {
+        static void Main(string[] args)
+        {
+            var input = Console.ReadLine().Split(' ').ToList();
+            var reversedArray = new List<int>();
+
+            for (int i = 0; i < input.Count; i++)
+            {
+                var element = input[i];
+                var reversedElement = element.ToCharArray().Reverse().ToArray();
+                reversedArray.Add(int.Parse(string.Join("", reversedElement)));
+            }
+            Console.WriteLine(reversedArray.Sum());
+        }
+    }
+}
