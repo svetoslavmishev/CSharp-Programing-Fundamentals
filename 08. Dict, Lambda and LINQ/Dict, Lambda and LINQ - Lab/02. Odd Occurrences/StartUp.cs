b477119755fae3341ab260a7a9c5de6270741dae
svetoslavmishev <svetli>
2017-12-17 16:30:28 +0200

    add lab dict

---
+++ 08. Dict, Lambda and LINQ/Dict, Lambda and LINQ - Lab/02. Odd Occurrences/StartUp.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _02.Odd_Occurrences
+{
+    class StartUp
+    {
+        static void Main()
+        {
+            string input = Console.ReadLine().ToLower();
+            string[] words = input.Split(' ');
+            Dictionary<string, int> count = new Dictionary<string, int>();
+
+            foreach (var word in words)
+            {
+                if (count.ContainsKey(word))
+                {
+                    count[word]++;
+                }
+                else
+                {
+                    count[word] = 1;
+                }
+            }
+            List<string> output = new List<string>();
+
+            foreach (var pair in count)
+            {
+                if (pair.Value % 2 != 0)
+                {
+                    output.Add(pair.Key);
+                }
+            }
+            Console.WriteLine(string.Join(", ", output));
+        }
+    }
+}
---
+++ 08. Dict, Lambda and LINQ/Dict, Lambda and LINQ - Lab/06. Fold and Sum/StartUp.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _06.Fold_and_Sum
+{
+    class StartUp
+    {
+        static void Main()
+        {
+            int[] input = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            int[] first = input.Take(input.Length / 4).Reverse().ToArray();
+            int[] second = input.Reverse().Take(input.Length / 4).ToArray();
+            int[] firstRow = first.Concat(second).ToArray();
+            int[] secondRow = input.Skip(input.Length / 4).Take(input.Length / 2).ToArray();
+
+            var total = firstRow.Select((x, index) => x + secondRow[index]);
+
+            Console.WriteLine(string.Join(" ", total));
+        }
+    }
+}
