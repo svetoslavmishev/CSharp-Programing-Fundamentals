7a02bf95a74d2f9f7cfdc1144ca0b452335109a8
svetoslavmishev <svetli>
2017-12-17 16:10:44 +0200

    add 8

---
+++ 08. Dict, Lambda and LINQ/Dict, Lambda and LINQ - Lab/04. Largest 3 Numbers/StartUp.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _04.Largest_3_Numbers
+{
+    class StartUp
+    {
+        static void Main()
+        {
+            var input = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+            var getLargest3 = input.OrderByDescending(x => x).Take(3);
+            Console.WriteLine(string.Join(" ", getLargest3));
+        }
+    }
+}
