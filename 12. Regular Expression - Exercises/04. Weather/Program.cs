5227c328c941033871f50bac84c1ec234ba429f2
svetoslavmishev <svetlioo.m@gmail.com>
2018-01-10 20:00:55 +0200

    add last two exercises

---
+++ 12. Regular Expression - Exercises/04. Weather/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace _04.Weather
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+
+            Dictionary<string, double> cityAndTemperature =
+                new Dictionary<string, double>();
+            Dictionary<string, string> cityAndWeather =
+                new Dictionary<string, string>();
+
+            string pattern = @"([A-Z]{2})(\d+\.\d+)([a-zA-Z]+)\|";
+
+            while (input != "end")
+            {
+                if (Regex.IsMatch(input, pattern))
+                {
+                    Match match = Regex.Match(input, pattern);
+                    string nameOfTheCity = match.Groups[1].Value;
+                    double averageTemperature = double.Parse(match.Groups[2].Value);
+                    string typeOfWeather = match.Groups[3].Value;
+
+                    cityAndTemperature[nameOfTheCity] = averageTemperature;
+                    cityAndWeather[nameOfTheCity] = typeOfWeather;
+                }
+
+                input = Console.ReadLine();
+            }
+
+            Dictionary<string, double> temperatureSortedSity =
+                cityAndTemperature.OrderBy(c => c.Value).ToDictionary(x => x.Key, x => x.Value);
+
+            foreach (var sortCity in temperatureSortedSity)
+            {
+                Console.WriteLine($"{sortCity.Key} => {sortCity.Value} => {cityAndWeather[sortCity.Key]}");
+            }
+        }
+    }
+}
---
+++ 12. Regular Expression - Exercises/05. Key Replacer/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace _05.Key_Replacer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            string key1 = @"^[a-zA-Z]+[<|\\]";
+            string key2 = @"[<|\\][a-zA-Z]+$";
+
+            var firstKeyMatch = Regex.Match(input, key1).ToString().TrimEnd('<', '|', '\\'); ;
+            var secondKeyMatch = Regex.Match(input, key2).ToString().TrimStart('<', '|', '\\'); ;
+                        
+            var pattern = $"{firstKeyMatch}(.*?){secondKeyMatch}";
+
+            var text = Console.ReadLine();
+
+            StringBuilder sb = new StringBuilder();
+
+            MatchCollection match = Regex.Matches(text, pattern);
+
+            foreach (Match m in match)
+            {
+                sb.Append(m.Groups[1].Value);
+            }
+
+            if (sb.Length > 1)
+            {
+                Console.WriteLine(sb.ToString());
+            }
+            else
+            {
+                Console.WriteLine("Empty result");
+            }
+        }
+    }
+}
